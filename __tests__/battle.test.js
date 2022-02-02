8ddea91c65a3b2ec148daef3423621facee9aab2
Mikah Mathews <mikah@outofband.org>
2022-02-02 13:20:41 -0800

    Add test 3 for thunderbolt attack
    Co-Authored-by: Tiffany Rodrigo <tiffa.rodrigs@gmail.com>

--- __tests__/battle.test.js
+++ __tests__/battle.test.js
@@ -2,25 +2,19 @@ import Battle from './../src/js/battle.js'
 
 describe('Battle', () =>{
 
-  test('Should create a battle object',() =>{
-    const battle = new Battle("wizard","10","orc","10");
+  test('Should create a battle object',() => {
+    const battle = new Battle("wizard",10,"orc",10);
     expect(battle.character1).toEqual("wizard");
-    expect(battle.ch1health).toEqual("10");
+    expect(battle.ch1health).toEqual(10);
     expect(battle.character2).toEqual("orc");
-    expect(battle.ch2health).toEqual("10");
-
-
-    // each character will hve different attacks
-    // wizard attacks:                 orc attacks:
-    // - thunderbolt (-2 health)       - mace (-2 health)
-    // - sword (-3 health)             - shoot arrow (-3 health)
-    //player
-    //playerHealth
-    //monster
-    //monsterHealth
-    //battle type
+    expect(battle.ch2health).toEqual(10);
   });
 
+  test('Should create thunderbolt attack',() => {
+    const battle = new Battle("wizard",10,"orc",10);
+    expect(battle.thunderbolt()).toEqual(8);
+    expect(battle.ch2health).toEqual(8);
+  });
 
 
 });
