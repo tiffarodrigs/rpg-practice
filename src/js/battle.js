d59ed3a0637e2fbbce67bb9488b6fd5c7dc2bec4
Mikah Mathews <mikah@outofband.org>
2022-02-02 13:24:16 -0800

    Add passing code for test 3(thunderbolt attack)
    Co-Authored-by: Tiffany Rodrigo <tiffa.rodrigs@gmail.com>

--- src/js/battle.js
+++ src/js/battle.js
@@ -5,5 +5,9 @@ export default class Battle{
     this.character2 = character2;
     this.ch2health = ch2health;
   }
+  thunderbolt(){
+    this.ch2health -= 2;
+    return this.ch2health;
+  } // sword, mace, shoot
 
 }
