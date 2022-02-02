10d76d202cc15d755b4e768b5aaf02844a44818f
Mikah Mathews <mikah@outofband.org>
2022-02-02 13:35:54 -0800

    Add test 5 for mace attack
    Co-Authored-by: Tiffany Rodrigo <tiffa.rodrigs@gmail.com>

--- __tests__/battle.test.js
+++ __tests__/battle.test.js
@@ -2,8 +2,12 @@ import Battle from './../src/js/battle.js'
 
 describe('Battle', () =>{
 
+  let battle
+  beforeEach(() => {
+    battle = new Battle("wizard",10,"orc",10);
+  });
+
   test('Should create a battle object',() => {
-    const battle = new Battle("wizard",10,"orc",10);
     expect(battle.character1).toEqual("wizard");
     expect(battle.ch1health).toEqual(10);
     expect(battle.character2).toEqual("orc");
@@ -11,16 +15,18 @@ describe('Battle', () =>{
   });
 
   test('Should create thunderbolt attack',() => {
-    const battle = new Battle("wizard",10,"orc",10);
     expect(battle.thunderbolt()).toEqual(9);
     expect(battle.ch2health).toEqual(9);
   });
 
   test('Should create sword attack',() => {
-    const battle = new Battle("wizard",10,"orc",10);
     expect(battle.sword()).toEqual(8);
     expect(battle.ch2health).toEqual(8);
   });
   
+  test('Should create mace attack', () => {
+    expect(battle.mace()).toEqual(9);
+    expect(battle.ch1health).toEqual(9);
+  });
 
 });
